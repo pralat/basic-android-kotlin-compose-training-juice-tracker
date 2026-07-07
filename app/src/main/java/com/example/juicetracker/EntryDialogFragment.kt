1a85e5ff41f02e8cf254f68e9d5fe59dcea33349
Mark Pralat <mpralat@gmail.com>
2026-07-07 12:06:39 -0400

    Step 5. Modify the navigation component.

---
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -0,0 +1,29 @@
+package com.example.juicetracker
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.juicetracker.databinding.FragmentEntryDialogBinding
+import com.example.juicetracker.ui.AppViewModelProvider
+import com.example.juicetracker.ui.EntryViewModel
+
+
+class EntryDialogFragment : BottomSheetDialogFragment() {
+
+    private val entryViewModel by viewModels<EntryViewModel> { AppViewModelProvider.Factory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentEntryDialogBinding.inflate(inflater, container, false).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = FragmentEntryDialogBinding.bind(view)
+    }
+}
