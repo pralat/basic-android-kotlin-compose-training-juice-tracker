f69c8336354f9911630d18b2e317a44bcbe5fbf9
Mark Pralat <mpralat@gmail.com>
2026-07-08 14:43:14 -0400

    Add Save Button and Color Dropwdown logic.

--- app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.juicetracker.data.JuiceColor
@@ -31,6 +34,25 @@ class EntryDialogFragment : BottomSheetDialogFragment() {
         val binding = FragmentEntryDialogBinding.bind(view)
         val args: EntryDialogFragmentArgs by navArgs()
         val juiceId = args.itemId
+
+        binding.name.doAfterTextChanged { text ->
+            binding.saveButton.isEnabled = !text.isNullOrBlank()
+        }
+
+        binding.colorSpinner.adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            JuiceColor.entries.map { getString(it.label) }
+        )
+        binding.colorSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedColor = JuiceColor.entries[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                selectedColor = JuiceColor.Red
+            }
+        }
 
         binding.saveButton.setOnClickListener {
             entryViewModel.saveJuice(
