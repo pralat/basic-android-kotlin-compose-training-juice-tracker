40cf854d953f5cd109b74e7a40eeecc19015e606
Mark Pralat <mpralat@gmail.com>
2026-07-14 11:19:52 -0400

    Add the Compose library to the app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
@@ -53,10 +54,14 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        // Enable Jetpack Compose for this module
+        compose = true
     }
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
@@ -71,5 +76,12 @@ dependencies {
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.12.0")
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -27,5 +27,6 @@ plugins {
     id("com.android.application") version "8.11.1" apply false
     id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("androidx.navigation.safeargs") version "2.9.3" apply false
 }
